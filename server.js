be96e9794a42c2d4cd08ad6f2925418b9d1bd5fe
dannycahyo <dannydwicahyono@gmail.com>
2023-08-18 06:58:17 +0700

    feat: enable server side routing

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const path = require("path");
 
 const PORT = 3000;
 
-const server = http.createServer((_req, res) => {
-  const filePath = _req.url === "/" ? "index.html" : _req.url;
+const server = http.createServer((req, res) => {
+  const filePath =
+    req.url === "/" || req.url === "/product" ? "index.html" : req.url;
   const fileExtension = path.extname(filePath);
 
   let contentType;
@@ -21,15 +22,14 @@ const server = http.createServer((_req, res) => {
       contentType = "text/html";
   }
 
-  res.statusCode = 200;
   res.setHeader("Content-Type", contentType);
 
   fs.readFile(path.join(__dirname, filePath), (err, data) => {
-    console.log({ data });
     if (err) {
       res.statusCode = 404;
       res.end("File not found");
     } else {
+      res.statusCode = 200;
       res.end(data);
     }
   });
