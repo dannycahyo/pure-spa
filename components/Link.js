72d91569d378470f0c37409bc2f1ebc3649a1a17
dannycahyo <dannydwicahyono@gmail.com>
2023-08-20 13:52:35 +0700

    fix: change the client side routing element implementation from button into link

--- components/Link.js
+++ components/Link.js
@@ -2,25 +2,22 @@ import { setState } from "../index.js";
 import { routes } from "../routes.js";
 
 const Link = ({ href, label }) => {
-  let element;
-
   const currentRoute = routes.find((route) => route.path === href);
 
   if (currentRoute) {
-    if (currentRoute.isServerPage) {
-      element = document.createElement("a");
-      element.href = href;
-      element.textContent = label;
-    } else {
-      element = document.createElement("button");
-      element.textContent = label;
-      element.onclick = () => {
+    const element = document.createElement("a");
+    element.href = href;
+    element.textContent = label;
+    element.onclick = (event) => {
+      if (!currentRoute.isServerPage) {
+        event.preventDefault();
         setState({ pathURLName: currentRoute.path });
-      };
-    }
+      }
+    };
+    return element;
+  } else {
+    return null;
   }
-
-  return element;
 };
 
 export default Link;
